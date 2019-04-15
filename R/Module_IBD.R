e869ae5d5c888503561334c392a20ab2911191e8
sebastiz <sebastiz@hotmail.com>
2019-04-15 20:16:09 +0000

    IBD_Module plans...

---
+++ R/Module_IBD.R
@@ -0,0 +1,28 @@
+#Development of the IBD Module scripts:
+
+# Aims:
+
+# 1.              Level of training of senior endoscopist: consultant/junior/nurse endoscopist YES
+# 3.    ·         Specific indication for procedure YES
+# 7.    ·         Adequate description of degree of endoscopic disease activity in UC using Mayo endoscopic score YES
+# 8.    ·         Adequate description of degree of endoscopic disease activity in UC using UCEIS endoscopy score YES
+# 
+# 
+# EXAMPLE DATASET NEEDED
+# 2.    ·         General description of previous disease extent (anywhere on the report) – NEED EXAMPLE DATASET
+# 10.    ·         Overall impression/summary of findings is provided NEED EXAMPLE DATASET
+# 4.    ·         Specific IBD therapy at the time of procedure (medication and route (unless obvious) listed but no need for dosage/interval) NEED EXAMPLE DATASET
+# 5.    ·         Description of UC related symptoms at the time of current evaluation included in the report (consider ‘yes’ if any of the following documented: diarrhoea, nocturnal bowel opening, urgency, abdominal pain, PR bleeding, fatigue, extraintestinal features of IBD, or a statement describing their absence e.g. ‘asymptomatic’) NEED EXAMPLE DATASET.... Need symptom list from somewhere...
+# 
+# 
+# BITERM ANALYSIS
+# 6.    ·         Description of maximal extent of endoscopically visible disease BITERM ANALYSIS 
+# 9.    ·         Description of segmental location of biopsies obtained BITERM ANALYSIS
+# 
+# TEMPORAL POS TAGGING
+# 11.    ·         Any statement describing recommendations for treatment alteration/continuation TEMPORAL POS TAGGING
+# 12.    ·         Instructions on when and how to follow-up  ?FROM FOLLOW-UP ONLY?
+#   
+#   MISSING
+# 13.    ·         Speciality of senior endoscopist: Physician/Surgeon NO DETAILS FROM THIS REPORT
+  
